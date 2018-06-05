a79510c91dc6d4b79d0a33ce44dc5d8450e75eb1
Linh Bui <builinh@weiser.cs.grinnell.edu>
2018-06-05 21:27:47 +0000

    changed matrix test to malloc

--- data-collection/simpletest/matrixmultiplier.c
+++ data-collection/simpletest/matrixmultiplier.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
- 
+#include <stdlib.h>
+
 int main()
 {
   int num_row1, num_col1, num_row2, num_col2, sum = 0;
@@ -7,20 +8,29 @@ int main()
   printf("Enter the number of rows and columns of first matrix\n");
   scanf("%d%d", &num_row1, &num_col1);
   printf("Enter the elements of first matrix\n");
-  int first[num_row1][num_col1]; 
+  int ** first = (int **)malloc(num_row1 * sizeof(int *));
+    for (int i=0; i<num_row1; i++)
+         first[i] = (int *)malloc(num_col1 * sizeof(int));
+
   for (int i = 0 ; i < num_row1 ; i++ )
     for (int j = 0 ; j < num_col1 ; j++ )
       scanf("%d", &first[i][j]);
 
   printf("Enter the number of rows and columns of second matrix\n");
   scanf("%d%d", &num_row2, &num_col2);
-  int second[num_row2][num_col2];
- 
+  int ** second = (int **)malloc(num_row2 * sizeof(int *));
+    for (int i=0; i<num_row2; i++)
+         second[i] = (int *)malloc(num_col2 * sizeof(int));
+
+
   if (num_col1 != num_row2)
     printf("Matrices with entered orders can't be multiplied with each other.\n");
   else
   {
-    int multiply[num_row1][num_col2];
+    int ** multiply = (int **)malloc(num_row1 * sizeof(int *));
+    for (int i=0; i<num_row1; i++)
+         multiply[i] = (int *)malloc(num_col2 * sizeof(int));
+
     printf("Enter the elements of second matrix\n");
  
     for (int i = 0 ; i < num_row2 ; i++ )
@@ -51,6 +61,7 @@ int main()
       printf("\n");
     }
   }
- 
+  free(first);
+  free(second);	
   return 0;
 }
