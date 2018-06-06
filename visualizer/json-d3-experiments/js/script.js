2206b3a61e8f4695b9b4d38b8d6bce31a119249d
zoerosemarie <grubbs.zoe@gmail.com>
2018-06-06 10:53:16 -0500

    Fixed points and added comments

--- visualizer/json-d3-experiments/js/script.js
+++ visualizer/json-d3-experiments/js/script.js
@@ -1,27 +1,33 @@
+// Set size and margins of graph
 var width = 940,
     height = 300,
     pad = 20,
     left_pad = 100;
 
+// Create an svg object for the graph
 var svg = d3.select("#plot")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-d3.json("./js/sample-data.json", function(d) {
+// Load the data from the JSON file
+d3.json("./data/3-objects.json", function(d) {
 
+  // Calculate size of x-axis based on number of data points
   var xAxisRange = d3.max(d, function(d) {
-    return d.time;
+    return d.instruction;
   });
 
+  // Create functions to scale objects vertically and horizontally according to the size of the graph
   var x = d3.scale.linear().domain([0, xAxisRange]).range([left_pad, width - pad]),
       y = d3.scale.linear().domain([1, 0]).range([pad, height - pad * 2]);
 
+  // Create axes and format the ticks on the y-axis as percentages
   var formatAsPercentage = d3.format(".1%");
   var xAxis = d3.svg.axis().scale(x).orient("bottom"),
       yAxis = d3.svg.axis().scale(y).orient("left").tickFormat(formatAsPercentage);
 
-
+  // Add the axes to the svg object
   svg.append("g")
     .attr("class", "axis")
     .attr("transform", "translate(0, " + (height - pad) + ")")
@@ -32,28 +38,19 @@ d3.json("./js/sample-data.json", function(d) {
     .attr("transform", "translate(" + (left_pad - pad) + ", 0)")
     .call(yAxis);
 
-  
-
-  for (var i = 0; i < d.length; i++) {
-    console.log(d[i]);
-  };
-
+  // Create the points and position them in the graph
   svg.selectAll("circle")
     .data(d)
     .enter()
     .append("circle")
     .attr("cx", function(p, i) {
-      var temp = x(d[i].time);
-      console.log("x value: ");
-      console.log(temp);
-      console.log("i: " + i);
-      return temp;
+      return x(d[i].instruction);
     })
     .attr("cy", function(p, i) {
-      var temp = y(d[i].missRate);
-      console.log(temp);
-      return temp;
+      var changeInHit = d[i - 1].hit - d[i].hit;
+      var changeInMiss = d[i - 1].miss - d[i].miss;
+      return y(changeInMiss / (changeInHit + changeInMiss));
     })
-    .attr("r", 3);
+    .attr("r", 1);
   
 });
