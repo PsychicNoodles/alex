0181732d43601d734e9d607cbd8013b5b82370b2
zoerosemarie <grubbs.zoe@gmail.com>
2018-06-06 10:04:21 -0500

    Added points but still broken

--- visualizer/json-d3-experiments/js/script.js
+++ visualizer/json-d3-experiments/js/script.js
@@ -8,36 +8,51 @@ var svg = d3.select("#plot")
     .attr("width", width)
     .attr("height", height);
 
-var x = d3.scale.linear().domain([0, 4]).range([left_pad, width - pad]),
-    y = d3.scale.linear().domain([99, 0]).range([pad, height - pad * 2]);
+d3.json("./js/sample-data.json", function(d) {
 
-var xAxis = d3.svg.axis().scale(x).orient("bottom"),
-    yAxis = d3.svg.axis().scale(y).orient("left");
+  var xAxisRange = d3.max(d, function(d) {
+    return d.time;
+  });
+
+  var x = d3.scale.linear().domain([0, xAxisRange]).range([left_pad, width - pad]),
+      y = d3.scale.linear().domain([1, 0]).range([pad, height - pad * 2]);
+
+  var formatAsPercentage = d3.format(".1%");
+  var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+      yAxis = d3.svg.axis().scale(y).orient("left").tickFormat(formatAsPercentage);
 
 
-svg.append("g")
+  svg.append("g")
     .attr("class", "axis")
     .attr("transform", "translate(0, " + (height - pad) + ")")
     .call(xAxis);
  
-svg.append("g")
+  svg.append("g")
     .attr("class", "axis")
     .attr("transform", "translate(" + (left_pad - pad) + ", 0)")
     .call(yAxis);
 
-d3.json("./js/sample-data.json", function(d) {
+  
 
-  console.log(d[1]);
+  for (var i = 0; i < d.length; i++) {
+    console.log(d[i]);
+  };
 
   svg.selectAll("circle")
     .data(d)
     .enter()
     .append("circle")
-    .attr("cx", function(d, i) {
-      return d[0].time;
+    .attr("cx", function(p, i) {
+      var temp = x(d[i].time);
+      console.log("x value: ");
+      console.log(temp);
+      console.log("i: " + i);
+      return temp;
     })
-    .attr("cy", function(d, i) {
-      return height - (d[1].missRate);
+    .attr("cy", function(p, i) {
+      var temp = y(d[i].missRate);
+      console.log(temp);
+      return temp;
     })
     .attr("r", 3);
   
