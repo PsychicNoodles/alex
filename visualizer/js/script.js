a6b989b03b885456e923e5ed072372fd76d50d29
zoerosemarie <grubbs.zoe@gmail.com>
2018-06-07 09:38:01 -0500

    Adjusted for new data format and added switch statement

--- visualizer/js/script.js
+++ visualizer/js/script.js
@@ -1,6 +1,6 @@
 // Set size and margins of graph
-var width = 940,
-    height = 300,
+var width = 1000,
+    height = 500,
     pad = 20,
     left_pad = 100;
 
@@ -15,18 +15,34 @@ d3.json("./data/percentageTest.json", function(results) {
 
   var dataset = results.timeslice;
 
+  for(var i = 0; i < dataset.length; i++) {
+    var cur = dataset[i];
+    for(var j = 0; j < cur.events.length; j++) {
+      switch(cur.events[j].name) {
+        case "cache":
+          cache(dataset, i);
+          break;
+        case "power":
+          power(dataset, i);
+          break;
+        case "branchPredictor":
+          branchPredictor(dataset, i);
+          break;
+      }
+    }
+  }
+
+  // Calculate counts and percentages of cache hits and misses and add them to new fields in the array (calculate the first element separately)
   var firstElem = dataset[0];
-  firstElem.hitsTemp = firstElem.events.cache.hits;
-  //console.log(firstElem.events.cache.hits)
-  console.log(firstElem.hitsTemp);
-  firstElem.missesTemp = firstElem.events.cache.misses;
+  firstElem.hitsTemp = firstElem.events[0].hits;
+  firstElem.missesTemp = firstElem.events[0].misses;
   firstElem.hitsPerc = firstElem.hitsTemp / (firstElem.missesTemp + firstElem.hitsTemp);
   firstElem.missesPerc = firstElem.missesTemp / (firstElem.missesTemp + firstElem.hitsTemp);
-
-  for(i = 1; i < dataset.length; i++) {
+  
+  for(var i = 1; i < dataset.length; i++) {
     var cur = dataset[i];
-    cur.hitsTemp = cur.events.cache.hits - dataset[i - 1].events.cache.hits;
-    cur.missesTemp = cur.events.cache.misses - dataset[i - 1].events.cache.misses;
+    cur.hitsTemp = cur.events[0].hits - dataset[i - 1].events[0].hits;
+    cur.missesTemp = cur.events[0].misses - dataset[i - 1].events[0].misses;
     cur.hitsPerc = cur.hitsTemp / (cur.missesTemp + cur.hitsTemp);
     cur.missesPerc = cur.missesTemp / (cur.missesTemp + cur.hitsTemp);
   }
@@ -41,7 +57,7 @@ d3.json("./data/percentageTest.json", function(results) {
       y = d3.scale.linear().domain([1, 0]).range([pad, height - pad * 2]);
 
   // Create axes and format the ticks on the y-axis as percentages
-  var formatAsPercentage = d3.format(".1%");
+  var formatAsPercentage = d3.format("%");
   var xAxis = d3.svg.axis().scale(x).orient("bottom"),
       yAxis = d3.svg.axis().scale(y).orient("left").tickFormat(formatAsPercentage);
 
@@ -55,9 +71,6 @@ d3.json("./data/percentageTest.json", function(results) {
     .attr("class", "axis")
     .attr("transform", "translate(" + (left_pad - pad) + ", 0)")
     .call(yAxis);
-
-  var prevHit = 0;
-  var prevMiss = 0;
 
   // Create the points and position them in the graph
   svg.selectAll("circle")
@@ -74,3 +87,15 @@ d3.json("./data/percentageTest.json", function(results) {
     .attr("r", 3);
   
 });
+
+function cache(dataset, i) {
+  console.log("In cache function, i: ", i)
+}
+
+function power(dataset, i) {
+  console.log("In power function, i: ", i)
+}
+
+function branchPredictor(dataset, i) {
+  console.log("In branchPredictor function, i: ", i)
+}
