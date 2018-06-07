be2b577619c4bd10a443b4c54a7763258a64cdd8
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-06-07 16:07:41 -0500

    Fold alex.h into alex.cpp

--- data-collection/alex.cpp
+++ data-collection/alex.cpp
@@ -1,7 +1,70 @@
-#include "alex.h"
+#include <assert.h>
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <linux/perf_event.h>
+#include <perfmon/perf_event.h>
+#include <perfmon/pfmlib.h>
+#include <perfmon/pfmlib_perf_event.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/syscall.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <unordered_map>
+#include <elf.h>
+#include <map>
+#include <string>
+#include <vector>
+
 #include "debug.hpp"
 
+using std::string;
+using std::vector;
+
+#define PAGE_SIZE 0x1000LL
+// this needs to be a power of two :'( (an hour was spent here)
+#define NUM_DATA_PAGES 256
+#define EVENT_ACCURACY 10000000
+#define SAMPLE_TYPE 0  // (PERF_SAMPLE_CALLCHAIN)
+
+// kill failure. Not really a fail but a security hazard.
+#define KILLERROR 1  // Cannot kill parent
+#define FORKERROR 2  // Cannot fork
+#define OPENERROR 3  // Cannot open file
+#define PERFERROR 4  // Cannot make perf_event
+#define INSTERROR 5  // Cannot make fd for inst counter
+#define ASYNERROR 6  // Cannot set file to async mode
+#define FISGERROR 7  // Cannot set signal to file
+#define OWNERROR 8   // Cannot set file to owner
+#define SETERROR 9   // Cannot empty sigset
+#define ADDERROR 10  // Cannot add to sigset
+#define BUFFERROR 11 // Cannot open buffer
+#define SEMERROR 12  // Semaphore failed
+
 #define ALEX_VERSION "0.0.1"
+
+struct sample {
+  uint64_t num_instruction_pointers;
+  uint64_t instruction_pointers[];
+};
+
+typedef int (*main_fn_t)(int, char **, char **);
+
+int ppid;
+int cpid;
+FILE *writef;
+size_t init_time;
+static main_fn_t real_main;
+void *buffer;
+sem_t *child_sem, *parent_sem;
 
 /*
  * Reports time since epoch in milliseconds.
