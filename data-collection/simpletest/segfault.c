ccd656e94871a14264b5effd4316206469061da3
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2018-06-11 15:26:45 -0500

    reformatted source files

--- data-collection/simpletest/segfault.c
+++ data-collection/simpletest/segfault.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int main( int argc, char* argv[] )
-{
-  const char *s = NULL;
-  printf( "%c\n", s[0] );
+int main(int argc, char* argv[]) {
+  const char* s = NULL;
+  printf("%c\n", s[0]);
   return 0;
 }
