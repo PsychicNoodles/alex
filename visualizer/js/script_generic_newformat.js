d6ab497d335f562569901a65510d6f70af7bba47
Zoe Grubbs <grubbszo@amdahl.cs.grinnell.edu>
2018-06-12 15:21:04 -0500

    Added selection of points and table to display them

--- visualizer/js/script_generic_newformat.js
+++ visualizer/js/script_generic_newformat.js
@@ -1,6 +1,6 @@
 // Set size and margins of graph
-var width = 1,
-  height = 1,
+var width = 700,
+  height = 350,
   verticalPad = 20,
   horizontalPad = 100;
 
@@ -21,51 +21,56 @@ function loadFile() {
   }
 }
 
-/* Make sure the graph before drawing (prevents new input getting layered on
+/* Make sure the graph is clear before drawing (prevents new input getting layered on
     old input) */
 svg.selectAll("*").remove();
 
-/* converse the file into the array we use for visualization */
-function parseFile() {
-  var timeslices = JSON.parse(reader.result).timeslices;
-  densityInfo(timeslices);
+var timeslices;
 
-  //scatterPlot(timeslices);
+/* convert the file into the array we use for visualization */
+function parseFile() {
+  timeslices = JSON.parse(reader.result).timeslices;
+  //densityInfo(timeslices);
+
+  scatterPlot(timeslices);
 }
 
-/*Lets users to choose which resource they want the tool to present and analyze on */
+/* Let users choose which resource they want the tool to present and analyze */
 function chooseXAxis() {
   //need work!
   return "numInstructions";
 }
 
-/*Lets users to choose which resource they want the tool to present and analyze on */
+/* Let users choose which resource they want the tool to present and analyze */
 function chooseResource() {
   //need work!
   return "cache";
 }
 
 /* This function will take the raw array and a string of the specified resource and categorized them into cache, power, branchPredictor, etc */
-function categorizeEvents(timeslices, resourse) {
-  switch (resourse) {
+function categorizeEvents(timeslices, resource) {
+  switch (resource) {
     case "numInstructions":
       timeslices[0].instructionsAcc = timeslices[0].numInstructions;
+      timeslices[0].totalCycles = timeslices[0].numCPUCycles;
       for (var i = 1; i < timeslices.length; i++) {
         var cur = timeslices[i];
-        cur.instructionsAcc =
-          cur.numInstructions + timeslices[i - 1].instructionsAcc;
+        cur.totalCycles = cur.numCPUCycles + timeslices[i - 1].totalCycles;
+        cur.instructionsAcc = cur.numInstructions + timeslices[i - 1].instructionsAcc;
+        cur.selected = false;
       }
     case "cache":
-      for (var i = 0; i < timeslices.length; i++) {
+      timeslices[0].totalCycles = timeslices[0].numCPUCycles;
+      for (var i = 1; i < timeslices.length; i++) {
         var cur = timeslices[i];
-        var total =
-          cur.events["MEM_LOAD_RETIRED.L3_MISS"] +
-          cur.events["MEM_LOAD_RETIRED.L3_HIT"];
+        var total = cur.events["MEM_LOAD_RETIRED.L3_MISS"] + cur.events["MEM_LOAD_RETIRED.L3_HIT"];
         if (total == 0) {
           cur.events.missRates = 0;
         } else {
           cur.events.missRates = cur.events["MEM_LOAD_RETIRED.L3_MISS"] / total;
         }
+        cur.totalCycles = cur.numCPUCycles + timeslices[i - 1].totalCycles;
+        cur.selected = false;
       }
       break;
     case "power":
@@ -74,11 +79,6 @@ function categorizeEvents(timeslices, resourse) {
     case "branchPredictor":
       branchPredictor(timeslices, i);
       break;
-  }
-
-  for (var i = 0; i < timeslices.length; i++) {
-    var cur = timeslices[i];
-    for (var j = 0; j < cur.events.length; j++) { }
   }
 }
 
@@ -94,103 +94,52 @@ function findMax(timeslices, attr) {
 }
 
 function drawAxes(timeslices, xScale, yScale) {
-  // // Create axes and format the ticks on the y-axis as percentages
-  // var formatAsPercentage = d3.format(".0%");
-  // var abbrev = d3.format(".0s");
-  // var xAxis = d3.axisBottom(xScale).tickFormat(abbrev),
-  //   yAxis = d3.axisLeft(yScale).tickFormat(formatAsPercentage);
+  // Create axes and format the ticks on the y-axis as percentages
+  var formatAsPercentage = d3.format(".0%");
+  var abbrev = d3.format(".0s");
+  var xAxis = d3.axisBottom(xScale).tickFormat(abbrev),
+    yAxis = d3.axisLeft(yScale).tickFormat(formatAsPercentage);
 
-  // // Add the axes to the svg object
-  // svg
-  //   .append("g")
-  //   .attr("id", "xAxis")
-  //   .attr("class", "axis")
-  //   .attr("transform", "translate(0, " + (height - verticalPad * 2) + ")")
-  //   .call(xAxis);
+  // Add the axes to the svg object
+  svg
+    .append("g")
+    .attr("id", "xAxis")
+    .attr("class", "axis")
+    .attr("transform", "translate(0, " + (height - verticalPad * 2) + ")")
+    .call(xAxis);
 
-  // svg
-  //   .append("g")
-  //   .attr("id", "yAxis")
-  //   .attr("class", "axis")
-  //   .attr("transform", "translate(" + (horizontalPad - verticalPad) + ", 0)")
-  //   .call(yAxis);
+  svg
+    .append("g")
+    .attr("id", "yAxis")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + (horizontalPad - verticalPad) + ", 0)")
+    .call(yAxis);
 
-  // // Add labels to the axes
-  // svg
-  //   .select("xAxis")
-  //   .append("text")
-  //   .attr("class", "x label")
-  //   .attr("text-anchor", "end")
-  //   .attr("x", width / 2 + horizontalPad)
-  //   .attr("y", height)
-  //   .text(chooseXAxis());
+  // Add labels to the axes
+  svg
+    .select("xAxis")
+    .append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width / 2 + horizontalPad)
+    .attr("y", height)
+    .text(chooseXAxis());
 
-  // svg
-  //   .select("yAxis")
-  //   .append("text")
-  //   .attr("class", "y label")
-  //   .attr("text-anchor", "end")
-  //   .attr("y", 6)
-  //   .attr("x", (-1 * (height - verticalPad)) / 2)
-  //   .attr("dy", ".75em")
-  //   .attr("transform", "rotate(-90)")
-  //   .text("Cache miss rate");
+  svg
+    .select("yAxis")
+    .append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("x", (-1 * (height - verticalPad)) / 2)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("Cache miss rate");
 }
 
+var circles;
+var brush;
 
-
-
-
-function scatterPlot(timeslices) {
-  // categorizeEvents(timeslices, chooseResource());
-  // categorizeEvents(timeslices, chooseXAxis());
-
-  // // Calculate size of x-axis based on number of data points
-  // var xAxisMax = timeslices[timeslices.length - 1].instructionsAcc;
-  // var yAxisMax = findMax(timeslices, chooseResource());
-
-  // /* Create functions to scale objects vertically and horizontally according to
-  // the size of the graph */
-  // var xScale = d3
-  //   .scaleLinear()
-  //   .domain([0, xAxisMax])
-  //   .range([horizontalPad, width - verticalPad]),
-  //   yScale = d3
-  //     .scaleLinear()
-  //     .domain([yAxisMax, 0])
-  //     .range([verticalPad, height - verticalPad * 3]);
-
-  // drawAxes(timeslices, xScale, yScale);
-
-  // // Create the points and position them in the graph
-  // svg
-  //   .selectAll("circle")
-  //   .data(timeslices)
-  //   .enter()
-  //   .append("circle")
-  //   .attr("cx", function (d) {
-  //     // console.log(xScale(d.numInstructions));
-  //     return xScale(d.instructionsAcc);
-  //   })
-  //   .attr(
-  //     "cy",
-
-  //     function (d) {
-  //       if (isNaN(yScale(d.events.missRates))) {
-  //         console.log("XXXXXXXX ", d.numInstructions);
-  //       }
-  //       return yScale(d.events.missRates);
-  //     }
-  //   )
-
-  //   .attr("r", 2);
-  
-
-
-
-
-
-/******************************************* selector selector selector************************************************************/
 // Re-center brush when the user clicks somewhere in the graph
 function brushcentered() {
   var dx = x(1) - x(0), // Use a fixed width when recentering.
@@ -200,10 +149,56 @@ function brushcentered() {
   d3.select(this.parentNode).call(brush.move, x1 > width ? [width - dx, width] : x0 < 0 ? [0, dx] : [x0, x1]);
 }
 
-// Select the region that was selected by the user
+// Create a table of the points selected by the brush
+function createTable() {
+  d3.selectAll(".row_data").remove();
+  d3.select("table").style("visibility", "visible");
+
+  var circlesSelected = d3.selectAll(".brushed").data();
+
+  if(circlesSelected.length > 0) {
+    timeslices.forEach(function(d) {
+      if(d.selected) {
+        var formatRate = d3.format(".1%");
+        var data = [d.totalCycles, d.events["MEM_LOAD_RETIRED.L3_MISS"], d.events["MEM_LOAD_RETIRED.L3_HIT"], formatRate(d.events.missRates)];
+      
+        d3.select("table")
+          .append("tr")
+          .attr("class", "row_data")
+          .selectAll("td")
+          .data(data)
+          .enter()
+          .append("td")
+          .attr("align", (d, i) => i == 0 ? "left" : "right")
+          .text(d => d);
+      }
+    });
+  }
+}
+
+// Re-color the circles in the region that was selected by the user
 function brushed() {
-  var extent = d3.event.selection.map(x.invert, x);
-  circle.classed("selected", function(d) { return extent[0] <= d[0] && d[0] <= extent[1]; });
+  if(d3.event.selection != null) {
+    circles.attr("class", "circle");
+    var brushArea = d3.brushSelection(this);
+
+    circles.filter(function () {
+      var cx = d3.select(this).attr("cx");
+      return brushArea[0] <= cx && cx <= brushArea[1]; 
+    })
+    .attr("class", "brushed");
+
+    for(var i = 0; i < timeslices.length; i++) {
+      timeslices[i].selected = false;
+    }
+    
+    timeslices.map(function (d) {
+      if(brushArea[0] <= xScale(d.totalCycles) && xScale(d.totalCycles) <= brushArea[1]) {
+        d.selected = true;
+      }
+    });
+  }
+
 }
 
 var x = d3.scaleLinear()
@@ -213,18 +208,57 @@ var x = d3.scaleLinear()
 var y = d3.scaleLinear()
     .range([height, 0]);
 
-var circle;
+var xScale, yScale;
 
-// Creates brush
-  var brush = d3.brushX()
+function scatterPlot(timeslices) {
+  categorizeEvents(timeslices, chooseResource());
+  categorizeEvents(timeslices, chooseXAxis());
+
+  // Calculate size of x-axis based on number of data points
+  var xAxisMax = timeslices[timeslices.length - 1].totalCycles;
+  var yAxisMax = findMax(timeslices, chooseResource());
+
+
+  // Create functions to scale objects vertically and horizontally according to
+    // the size of the graph
+  xScale = d3
+    .scaleLinear()
+    .domain([0, xAxisMax])
+    .range([horizontalPad, width - verticalPad]);
+  yScale = d3
+    .scaleLinear()
+    .domain([yAxisMax, 0])
+    .range([verticalPad, height - verticalPad * 3]);
+
+  drawAxes(timeslices, xScale, yScale);
+
+  // Create the points and position them in the graph
+  circles = svg.selectAll("circle")
+    .data(timeslices)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      // console.log(xScale(d.numInstructions));
+      return xScale(d.totalCycles);
+    })
+    .attr(
+      "cy",
+      function (d) {
+        if (isNaN(yScale(d.events.missRates))) {
+          return 0;
+        }
+        return yScale(d.events.missRates);
+      }
+    )
+    .attr("r", 2);
+  
+  // Create brush
+  brush = d3.brushX()
     .extent([[0, 0], [width, height]])
-    .on("start brush", brushed);
+    .on("brush", brushed)
+    .on("end", createTable);
 
-  // Adds brush to svg object
-  svg.append("g")
-    .attr("class", "brush")
-    .call(d3.brush().on("brush", brushed));
-
+  // Add brush to svg object
   svg.append("g")
       .call(brush)
       .call(brush.move, [3, 5].map(x))
@@ -233,8 +267,6 @@ var circle;
       .on("mousedown touchstart", brushcentered);
 
 }
-
-
 
 function quadTreeX(d) {
   return d.instructionsAcc;
