c07be3b7fdfe8087417163526b0bc26553feaac1
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-06-12 16:28:58 -0500

    Add more options to electron cli

--- electron/main.js
+++ electron/main.js
@@ -2,11 +2,41 @@ const { app, BrowserWindow } = require("electron");
 const yargs = require("yargs");
 const { spawn } = require("child_process");
 const fs = require("fs");
+const readline = require("readline");
 
 const { argv } = yargs
-  .usage("usage: alex -- <executable> [<executable args>...]")
-  .default("out", `out-${Date.now()}.log`)
-  .default("err", `err-${Date.now()}.log`);
+  .usage("usage: alex [OPTIONS] -- EXECUTABLE [EXECUTABLE_ARGS...]")
+  .describe("in", "The file to pipe into the stdin of EXECUTABLE.")
+  .option("out", {
+    description: "The file to pipe the stdout of EXECUTABLE into.",
+    default: `out-${Date.now()}.log`
+  })
+  .option("err", {
+    description: "The file to pipe the stderr of EXECUTABLE into.",
+    default: `err-${Date.now()}.log`
+  })
+  .option("result", {
+    description: "The file to pipe the performance results into.",
+    default: `result-${Date.now()}.json`
+  })
+  .option("visualize", {
+    description: "Where to visualize the results.",
+    choices: ["window", "no", "ask"],
+    default: "ask"
+  })
+  .option("period", {
+    description: "The period in CPU cycles",
+    type: "number",
+    default: 1000000
+  })
+  .option("events", {
+    alias: "e",
+    description: "A list of events to count.",
+    type: "array",
+    demandOption: true
+  });
+
+// Create child process
 
 const executable = argv._[0];
 const executableArgs = argv._.slice(1);
@@ -14,12 +44,14 @@ const executableArgs = argv._.slice(1);
 const dataCollector = spawn(executable, executableArgs, {
   env: {
     ...process.env,
-    ALEX_PERIOD: Math.pow(10, 7),
-    ALEX_EVENTS: "MEM_LOAD_RETIRED.L3_MISS",
-    LD_PRELOAD: "../data-collection/alex.so",
-    ALEX_RESULT_FILE: `./result-${Date.now()}.json`
+    ALEX_PERIOD: argv.period,
+    ALEX_EVENTS: argv.events.join(","),
+    ALEX_RESULT_FILE: argv.result,
+    LD_PRELOAD: "../data-collection/alex.so"
   }
 });
+
+// Pipe through inputs and outputs
 
 if (argv.in) {
   const fileStream = fs.createReadStream(argv.in);
@@ -69,7 +101,27 @@ dataCollector.on("exit", code => {
     console.error(errorCodes[code]);
   } else {
     console.info("Successfully collected data.");
-    createWindow();
+    console.info(`Results saved to ${argv.result}`);
+
+    if (argv.visualize === "window") {
+      createWindow();
+    } else if (argv.visualize === "ask") {
+      const interface = readline.createInterface(process.stdin, process.stdout);
+      interface.question(
+        "Would you like to see a visualization of the results (yes/no)? ",
+        answer => {
+          if (answer === "no") {
+            console.info("Exiting.");
+            app.quit();
+            process.exit();
+          } else {
+            createWindow();
+          }
+
+          interface.close();
+        }
+      );
+    }
   }
 });
 
