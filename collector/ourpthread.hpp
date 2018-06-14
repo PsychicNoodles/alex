c7c02271e277b69b27bde00d285328f26283fa26
Linh Bui <builinh@shannon.cs.grinnell.edu>
2018-06-14 11:38:12 -0500

    start pthread attempt

---
+++ collector/ourpthread.hpp
@@ -0,0 +1,24 @@
+#include <dlfcn.h>
+#include <linux/perf_event.h>
+#include <perfmon/perf_event.h>
+#include <perfmon/pfmlib.h>
+#include <perfmon/pfmlib_perf_event.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define EVENT "perf_count_hw_cache_misses"
+#define SAMPLE 0
+#define EVENT_ACCURACY 100000
+
+typedef int (*pthread_create_fn_t)(pthread_t *, const pthread_attr_t *,
+                                   void *(*)(void *), void *);
+typedef void *(*routine_fn_t)(void *);
+
+typedef struct disguise {
+  routine_fn_t victim;
+  void *args;
+} disguise_t;
+
