c3ace8ae9bb59eddd0e723e9a9dbc2f792205bca
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-06-14 10:16:39 -0500

    Auto display result file

--- visualizer/index.js
+++ visualizer/index.js
@@ -1,24 +1,45 @@
-const { app, BrowserWindow } = require("electron");
+//
+// visualizer/index.js
+// Electron app main file.
+//
+
+const { app, BrowserWindow, ipcMain } = require("electron");
+const fs = require("fs");
 
 const resultFile = process.argv[2];
-console.log(`Reading from ${resultFile}`);
+if (!resultFile) {
+  throw new Error("No result file specified.");
+}
+
+const result = JSON.parse(fs.readFileSync(resultFile).toString());
+
+ipcMain.on("result-request", event => {
+  event.sender.send("result", result);
+});
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
-// This event fires when Electron has finished initialization and is ready to
-// create browser windows. Some APIs can only be used after this event occurs.
-app.on("ready", () => {
-  win = new BrowserWindow({ width: 1000, height: 820 });
-  win.loadFile(`${__dirname}/index.html`);
+app
+  // This event fires when Electron has finished initialization and is ready to
+  // create browser windows. Some APIs can only be used after this event occurs.
+  .on("ready", async () => {
+    win = new BrowserWindow({ width: 1000, height: 820, show: false });
 
-  win.on("closed", () => {
-    // Dereference the window object so it can be garbage collected
-    win = null;
+    win.loadFile(`${__dirname}/index.html`);
+
+    win
+      .on("ready-to-show", () => {
+        win.show();
+      })
+
+      .on("closed", () => {
+        // Dereference the window object so it can be garbage collected
+        win = null;
+      });
+  })
+
+  .on("window-all-closed", () => {
+    app.quit();
   });
-});
-
-app.on("window-all-closed", () => {
-  app.quit();
-});
