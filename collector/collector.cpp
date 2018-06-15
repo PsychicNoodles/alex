8f20500598314a95b46f67816f0132f7fbb44f50
Linh Bui <builinh@shannon.cs.grinnell.edu>
2018-06-15 14:46:21 -0500

    hopefully fixed the comma problem

--- collector/collector.cpp
+++ collector/collector.cpp
@@ -320,10 +320,10 @@ int analyzer(int pid) {
       bool skip = false;
       for (int i = 0; i < perf_sample->num_instruction_pointers; i++) {
         if (is_callchain_marker(perf_sample->instruction_pointers[i])) {
-          //fprintf(writef, "return true\n");
           skip = true;
-          //continue;
+          continue;
         }
+      
 
         if (i > 0 && skip == false) {
           fprintf(writef, ",");
