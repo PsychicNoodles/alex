ad3e0ce57ebf736d3d380a2815e5d318926a105b
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2018-06-19 14:21:27 -0500

    moved getenv_safe to util

--- collector/debug.hpp
+++ collector/debug.hpp
@@ -22,13 +22,6 @@
 bool enable_segfault_trace();
 void disable_segfault_trace();
 
-static inline std::string getenv_safe(const char* var,
-                                      const char* fallback = "") {
-  const char* value = getenv(var);
-  if (!value) value = fallback;
-  return std::string(value);
-}
-
 void dump_die(const dwarf::die& node);
 void dump_line_table(const dwarf::line_table& lt);
 int dump_table_and_symbol(char* path);
--- collector/util.hpp
+++ collector/util.hpp
@@ -9,3 +9,10 @@ size_t time_ms();
 char* int_to_hex(uint64_t i);
 vector<string> str_split(string str, string delim);
 void shutdown(pid_t pid, FILE* writef, int code);
+
+static inline std::string getenv_safe(const char* var,
+                                      const char* fallback = "") {
+  const char* value = getenv(var);
+  if (!value) value = fallback;
+  return std::string(value);
+}
