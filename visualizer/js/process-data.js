1f73f8b51d8c5e2d57426864f2d6d4edfe4ddcfb
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-06-21 09:33:57 -0500

    Fix eslint format

--- visualizer/js/process-data.js
+++ visualizer/js/process-data.js
@@ -128,15 +128,14 @@ function processData(
 
   return dataWithDensity;
 }
+
 /**
  * Compares selected data's function use to that of the unselected data.
  * (at least according to my current understanding)
- * Also
- * @function
- * @param data: all data available, not just selected data
- * @returns: not sure yet!
+ *
+ * @param data - all data available, not just selected data
+ * @returns - not sure yet!
  */
-
 function chiSquared(data) {
   const selected = data.filter(datum => datum.selected);
   console.log(selected);
