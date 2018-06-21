f712fa29abe0601488847db529a75f33ecfe6106
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-06-21 09:34:42 -0500

    Actually fix JSDoc format

--- visualizer/js/process-data.js
+++ visualizer/js/process-data.js
@@ -133,8 +133,8 @@ function processData(
  * Compares selected data's function use to that of the unselected data.
  * (at least according to my current understanding)
  *
- * @param data - all data available, not just selected data
- * @returns - not sure yet!
+ * @param data all data available, not just selected data
+ * @returns not sure yet!
  */
 function chiSquared(data) {
   const selected = data.filter(datum => datum.selected);
