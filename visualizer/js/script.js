5bb480678307a00791a092effd498c2f94dcd182
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2018-07-03 17:39:00 -0500

    added progress bar that tracks result file reading

--- visualizer/js/script.js
+++ visualizer/js/script.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 const readline = require("readline");
 const d3 = require("d3");
 
+const ProgressBar = require("progressbar.js");
+const { PROGRESS_HEIGHT, PROGRESS_DIVISIONS } = require("./util");
+
 require("bootstrap");
 
 const { processData } = require("./process-data");
@@ -12,29 +15,61 @@ const functionRuntimes = require("./function-runtimes");
 ipcRenderer.send("result-request");
 ipcRenderer.on("result", (event, resultFile) => {
   let result = "",
-    rl;
+    bytesRead = 0,
+    bytesTotal,
+    rl,
+    bar;
   try {
+    bytesTotal = fs.statSync(resultFile)["size"];
+    const progressPoints = [...Array(PROGRESS_DIVISIONS).keys()].map(
+      n => bytesTotal * (n / PROGRESS_DIVISIONS)
+    );
+    bar = new ProgressBar.Line("#progress", {
+      strokeWidth: 4,
+      easing: "easeInOut",
+      color: "#FFEA82",
+      trailColor: "#eee",
+      trailWidth: 1,
+      svgStyle: { width: "100%", height: PROGRESS_HEIGHT },
+      text: {
+        style: {
+          color: "#999",
+          verticalAlign: "middle",
+          textAlign: "center"
+        },
+        autoStyleContainer: false
+      },
+      step: (state, bar) =>
+        bar.setText(
+          `Read in ${Math.round(bar.value() * 100) + "%"} of result file`
+        )
+    });
     rl = readline
       .createInterface({
         input: fs.createReadStream(resultFile)
       })
-      .on(
-        "line",
-        line =>
-          (result += line.replace(/([^"]+)|("[^"]+")/g, ($0, $1, $2) => {
-            if ($1) {
-              return $1.replace(/\s/g, "");
-            } else {
-              return $2;
-            }
-          }))
-      );
+      .on("line", line => {
+        result += line.replace(/([^"]+)|("[^"]+")/g, ($0, $1, $2) => {
+          if ($1) {
+            return $1.replace(/\s/g, "");
+          } else {
+            return $2;
+          }
+        });
+        bytesRead += line.length;
+        if (bytesRead > progressPoints[0]) {
+          bar.animate(bytesRead / bytesTotal);
+          progressPoints.shift();
+        }
+      });
   } catch (err) {
     alert(`Invalid result file: ${err.message}`);
     window.close();
   }
 
   rl.on("close", () => {
+    bar.animate(1.0);
+
     result = JSON.parse(result);
     const processedData = processData(result.timeslices, result.header);
 
--- visualizer/js/util.js
+++ visualizer/js/util.js
@@ -4,5 +4,12 @@
 
 const CHART_WIDTH = 500;
 const CHART_HEIGHT = 250;
+const PROGRESS_HEIGHT = "8px";
+const PROGRESS_DIVISIONS = 10;
 
-module.exports = { CHART_WIDTH, CHART_HEIGHT };
+module.exports = {
+  CHART_WIDTH,
+  CHART_HEIGHT,
+  PROGRESS_HEIGHT,
+  PROGRESS_DIVISIONS
+};
