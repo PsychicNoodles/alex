e3125dc8c661a60fa04f20f8348891616883b6dc
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-07-10 22:31:58 -0500

    Make source select a dropdown

--- visualizer/js/source-select.js
+++ visualizer/js/source-select.js
@@ -5,8 +5,14 @@ const { Store } = require("./store");
 const hiddenSourcesSubscription = d3.local();
 const hiddenSourcesStore = new Store([]);
 
+const dropdownIsOpen = d3.local();
+
 function render(root, { sources }) {
   root.classed("source-select", true);
+
+  if (root.property(dropdownIsOpen) === undefined) {
+    root.property(dropdownIsOpen, false);
+  }
 
   if (root.select(".source-select__button").empty()) {
     root.append("button").attr("class", "source-select__button");
@@ -16,12 +22,18 @@ function render(root, { sources }) {
     root.append("div").attr("class", "source-select__dropdown");
   }
 
-  root.select(".source-select__button").text("Showing All Sources");
+  root.classed("source-select--dropdown-open", root.property(dropdownIsOpen));
+
+  root.select(".source-select__button").on("click", () => {
+    root
+      .property(dropdownIsOpen, !root.property(dropdownIsOpen))
+      .classed("source-select--dropdown-open", root.property(dropdownIsOpen));
+  });
 
   const dropdownItemsSelection = root
     .select(".source-select__dropdown")
     .selectAll(".source-select__dropdown-item")
-    .data(sources);
+    .data(["All Sources", ...sources]);
 
   const dropdownItemsEnterSelection = dropdownItemsSelection
     .enter()
@@ -35,34 +47,63 @@ function render(root, { sources }) {
 
   dropdownItemsEnterSelection
     .append("span")
-    .attr("class", "source-select__file-name");
+    .attr("class", "source-select__file-name")
+    .text(source => source);
 
   hiddenSourcesStore.subscribeUnique(
     root,
     hiddenSourcesSubscription,
     hiddenSources => {
+      const showingAllSources = hiddenSources.length === 0;
+      const showingSomeSources = sources.some(
+        source => !hiddenSources.includes(source)
+      );
+      root
+        .select(".source-select__button")
+        .text(
+          `Showing ${
+            showingAllSources ? "All" : showingSomeSources ? "Some" : "No"
+          } Sources`
+        );
+
       dropdownItemsSelection
         .merge(dropdownItemsEnterSelection)
-        .each(function(source) {
-          d3.select(this)
-            .select(".source-select__checkbox")
-            .property("checked", !hiddenSources.includes(source))
-            .on("change", function() {
-              if (this.checked) {
-                hiddenSourcesStore.dispatch(hiddenSources =>
-                  hiddenSources.filter(hiddenSource => hiddenSource !== source)
-                );
-              } else {
-                hiddenSourcesStore.dispatch(hiddenSources => [
-                  ...hiddenSources,
-                  source
-                ]);
-              }
-            });
-
-          d3.select(this)
-            .select(".source-select__file-name")
-            .text(source);
+        .each(function(source, i) {
+          if (i === 0) {
+            // We are on the All checkbox
+            d3.select(this)
+              .select(".source-select__checkbox")
+              .property("checked", showingAllSources)
+              .property(
+                "indeterminate",
+                showingSomeSources && !showingAllSources
+              )
+              .on("change", function() {
+                if (this.checked) {
+                  hiddenSourcesStore.dispatch(() => []);
+                } else {
+                  hiddenSourcesStore.dispatch(() => sources);
+                }
+              });
+          } else {
+            d3.select(this)
+              .select(".source-select__checkbox")
+              .property("checked", !hiddenSources.includes(source))
+              .on("change", function() {
+                if (this.checked) {
+                  hiddenSourcesStore.dispatch(hiddenSources =>
+                    hiddenSources.filter(
+                      hiddenSource => hiddenSource !== source
+                    )
+                  );
+                } else {
+                  hiddenSourcesStore.dispatch(hiddenSources => [
+                    ...hiddenSources,
+                    source
+                  ]);
+                }
+              });
+          }
         });
     }
   );
