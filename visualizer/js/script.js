5b3198fb2cb4270dafff890e669a2bd75a0dc246
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-07-10 19:24:15 -0500

    Make "Parsing JSON" message show

--- visualizer/js/script.js
+++ visualizer/js/script.js
@@ -21,12 +21,11 @@ const PROGRESS_HEIGHT = "8px";
 const PROGRESS_DIVISIONS = 10;
 
 ipcRenderer.send("result-request");
-ipcRenderer.on("result", (event, resultFile) => {
-  let result = "",
-    bytesRead = 0,
-    bytesTotal,
-    rl,
-    bar;
+ipcRenderer.on("result", async (event, resultFile) => {
+  let resultString = "";
+  let bytesRead = 0;
+  let bytesTotal;
+  let rl, bar;
   try {
     bytesTotal = fs.statSync(resultFile)["size"];
     const progressPoints = [...Array(PROGRESS_DIVISIONS).keys()].map(
@@ -60,7 +59,7 @@ ipcRenderer.on("result", (event, resultFile) => {
         input: fs.createReadStream(resultFile)
       })
       .on("line", line => {
-        result += line.replace(/([^"]+)|("[^"]+")/g, ($0, $1, $2) => {
+        resultString += line.replace(/([^"]+)|("[^"]+")/g, ($0, $1, $2) => {
           if ($1) {
             return $1.replace(/\s/g, "");
           } else {
@@ -78,152 +77,153 @@ ipcRenderer.on("result", (event, resultFile) => {
     window.close();
   }
 
-  rl.on("close", () => {
-    // usually doesn't appear because JSON.parse takes up all the CPU
-    bar.set(1.0);
-    bar.setText("Parsing JSON...");
+  await new Promise(resolve => rl.on("close", resolve));
 
-    requestAnimationFrame(() => {
-      result = JSON.parse(result);
-      const processedData = processData(result.timeslices, result.header);
+  bar.set(1.0);
+  bar.setText("Parsing JSON...");
 
-      bar.destroy();
-      d3.select("#progress").classed("progress--visible", false);
+  // Wait two frames so the "Parsing JSON" message can show up
+  await new Promise(requestAnimationFrame);
+  await new Promise(requestAnimationFrame);
 
-      const { presets } = result.header;
-      const charts = [
-        {
-          presetsRequired: ["cache"],
-          yAxisLabel: "Cache Miss Rate",
-          yFormat: d3.format(".0%"),
-          getDependentVariable: d =>
-            getEventCount(d, presets.cache.misses) /
-              (getEventCount(d, presets.cache.hits) +
-                getEventCount(d, presets.cache.misses)) || 0
-        },
-        {
-          presetsRequired: ["cpu"],
-          yAxisLabel: "Instructions Per Cycle",
-          yFormat: d3.format(".2"),
-          getDependentVariable: d =>
-            getEventCount(d, presets.cpu.instructions) /
-              getEventCount(d, presets.cpu.cpuCycles) || 0
-        }
-      ].filter(({ presetsRequired }) =>
-        presetsRequired.every(presetName => presetName in presets)
-      );
+  const result = JSON.parse(resultString);
+  const processedData = processData(result.timeslices, result.header);
 
-      const xAxisLabel = "CPU Time Elapsed";
-      const getIndependentVariable = d => d.cpuTime - processedData[0].cpuTime;
+  bar.destroy();
+  d3.select("#progress").classed("progress--visible", false);
 
-      const xScaleMin = getIndependentVariable(processedData[0]);
-      const xScaleMax = getIndependentVariable(
-        processedData[processedData.length - 1]
-      );
-      const xScale = d3
-        .scaleLinear()
-        .domain([xScaleMin, xScaleMax])
-        .range([0, chart.WIDTH]);
+  const { presets } = result.header;
+  const charts = [
+    {
+      presetsRequired: ["cache"],
+      yAxisLabel: "Cache Miss Rate",
+      yFormat: d3.format(".0%"),
+      getDependentVariable: d =>
+        getEventCount(d, presets.cache.misses) /
+          (getEventCount(d, presets.cache.hits) +
+            getEventCount(d, presets.cache.misses)) || 0
+    },
+    {
+      presetsRequired: ["cpu"],
+      yAxisLabel: "Instructions Per Cycle",
+      yFormat: d3.format(".2"),
+      getDependentVariable: d =>
+        getEventCount(d, presets.cpu.instructions) /
+          getEventCount(d, presets.cpu.cpuCycles) || 0
+    }
+  ].filter(({ presetsRequired }) =>
+    presetsRequired.every(presetName => presetName in presets)
+  );
 
-      const yScalesByChart = new WeakMap();
-      const plotDataByChart = new WeakMap();
+  const xAxisLabel = "CPU Time Elapsed";
+  const getIndependentVariable = d => d.cpuTime - processedData[0].cpuTime;
 
-      for (const chartParams of charts) {
-        const { getDependentVariable } = chartParams;
-        const yScaleMax = d3.max(processedData, getDependentVariable);
-        const yScale = d3
-          .scaleLinear()
-          .domain([yScaleMax, 0])
-          .range([0, chart.HEIGHT]);
+  const xScaleMin = getIndependentVariable(processedData[0]);
+  const xScaleMax = getIndependentVariable(
+    processedData[processedData.length - 1]
+  );
+  const xScale = d3
+    .scaleLinear()
+    .domain([xScaleMin, xScaleMax])
+    .range([0, chart.WIDTH]);
 
-        const plotData = computeRenderableData({
-          data: processedData,
-          xScale,
-          yScale,
-          getIndependentVariable,
-          getDependentVariable
-        });
+  const yScalesByChart = new WeakMap();
+  const plotDataByChart = new WeakMap();
 
-        yScalesByChart.set(chartParams, yScale);
-        plotDataByChart.set(chartParams, plotData);
-      }
+  for (const chartParams of charts) {
+    const { getDependentVariable } = chartParams;
+    const yScaleMax = d3.max(processedData, getDependentVariable);
+    const yScale = d3
+      .scaleLinear()
+      .domain([yScaleMax, 0])
+      .range([0, chart.HEIGHT]);
 
-      const densityMax = charts.reduce(
-        (currentMax, chart) =>
-          Math.max(
-            currentMax,
-            d3.max(plotDataByChart.get(chart), d => d.densityAvg)
-          ),
-        0
-      );
-      const spectrum = d3.interpolateGreens;
+    const plotData = computeRenderableData({
+      data: processedData,
+      xScale,
+      yScale,
+      getIndependentVariable,
+      getDependentVariable
+    });
 
-      for (const chartParams of charts) {
-        const { getDependentVariable, yAxisLabel, yFormat } = chartParams;
-        d3.select("#charts")
-          .append("div")
-          .call(chart.render, {
-            timeslices: processedData,
-            getIndependentVariable,
-            getDependentVariable,
-            xAxisLabel,
-            yAxisLabel,
-            xScale,
-            yScale: yScalesByChart.get(chartParams),
-            yFormat,
-            plotData: plotDataByChart.get(chartParams),
-            densityMax,
-            spectrum
-          });
-      }
+    yScalesByChart.set(chartParams, yScale);
+    plotDataByChart.set(chartParams, plotData);
+  }
 
-      d3.select("#legend").call(legend.render, { densityMax, spectrum });
+  const densityMax = charts.reduce(
+    (currentMax, chart) =>
+      Math.max(
+        currentMax,
+        d3.max(plotDataByChart.get(chart), d => d.densityAvg)
+      ),
+    0
+  );
+  const spectrum = d3.interpolateGreens;
 
-      const sourcesSet = new Set();
-      processedData.forEach(timeslice => {
-        timeslice.stackFrames.forEach(frame => {
-          sourcesSet.add(frame.fileName);
-        });
+  for (const chartParams of charts) {
+    const { getDependentVariable, yAxisLabel, yFormat } = chartParams;
+    d3.select("#charts")
+      .append("div")
+      .call(chart.render, {
+        timeslices: processedData,
+        getIndependentVariable,
+        getDependentVariable,
+        xAxisLabel,
+        yAxisLabel,
+        xScale,
+        yScale: yScalesByChart.get(chartParams),
+        yFormat,
+        plotData: plotDataByChart.get(chartParams),
+        densityMax,
+        spectrum
       });
+  }
 
-      d3.select("#source-select").call(sourceSelect.render, {
-        sources: [...sourcesSet]
-      });
+  d3.select("#legend").call(legend.render, { densityMax, spectrum });
 
-      stream
-        .fromStreamables([
-          sourceSelect.hiddenSourcesStore.stream,
-          brushes.selectionStore.stream
-        ])
-        .pipe(
-          stream.map(([hiddenSources, { selections }]) =>
-            analyze(
-              processedData
-                .map(timeslice => {
-                  const x = xScale(getIndependentVariable(timeslice));
-                  return {
-                    ...timeslice,
-                    selected:
-                      selections.length === 0 ||
-                      selections.some(
-                        ({ range }) => range[0] <= x && x <= range[1]
-                      ),
-                    stackFrames: timeslice.stackFrames.filter(
-                      frame => !hiddenSources.includes(frame.fileName)
-                    )
-                  };
-                })
-                .filter(timeslice => timeslice.stackFrames.length)
-            )
-          )
-        )
-        .pipe(
-          stream.subscribe(({ functionList }) => {
-            d3.select("#function-runtimes").call(functionRuntimes.render, {
-              functionList
-            });
-          })
-        );
+  const sourcesSet = new Set();
+  processedData.forEach(timeslice => {
+    timeslice.stackFrames.forEach(frame => {
+      sourcesSet.add(frame.fileName);
     });
   });
+
+  d3.select("#source-select").call(sourceSelect.render, {
+    sources: [...sourcesSet]
+  });
+
+  stream
+    .fromStreamables([
+      sourceSelect.hiddenSourcesStore.stream,
+      brushes.selectionStore.stream
+    ])
+    .pipe(
+      stream.map(([hiddenSources, { selections }]) =>
+        analyze(
+          processedData
+            .map(timeslice => {
+              const x = xScale(getIndependentVariable(timeslice));
+              return {
+                ...timeslice,
+                selected:
+                  selections.length === 0 ||
+                  selections.some(
+                    ({ range }) => range[0] <= x && x <= range[1]
+                  ),
+                stackFrames: timeslice.stackFrames.filter(
+                  frame => !hiddenSources.includes(frame.fileName)
+                )
+              };
+            })
+            .filter(timeslice => timeslice.stackFrames.length)
+        )
+      )
+    )
+    .pipe(
+      stream.subscribe(({ functionList }) => {
+        d3.select("#function-runtimes").call(functionRuntimes.render, {
+          functionList
+        });
+      })
+    );
 });
