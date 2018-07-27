ca09c90e08686d9a5b5a6b51cace86f8593ac0fb
Mattori Birnbaum <mattori.birnbaum@gmail.com>
2018-07-27 16:14:12 -0500

    fixed protobuf-print not checking for eof

--- collector/protobuf-print.cpp
+++ collector/protobuf-print.cpp
@@ -56,6 +56,9 @@ int main(int argc, char **argv) {
 
   while (true) {
     if (!input.ReadVarint32(&size)) {
+      if (!input.ExpectAtEnd()) {
+        return 0;
+      }
       cerr << "failed to parse timeslice, couldn't read delimiter" << endl;
       return 3;
     }
@@ -77,6 +80,9 @@ int main(int argc, char **argv) {
 
   while (true) {
     if (!input.ReadVarint32(&size)) {
+      if (!input.ExpectAtEnd()) {
+        return 0;
+      }
       cerr << "failed to parse warning, couldn't read delimiter" << endl;
       return 4;
     }
