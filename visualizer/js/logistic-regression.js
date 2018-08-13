31f96c9e81bfa9f7e338471abd871eb8b8f8e353
Hadley Luker <lukerhad@grinnell.edu>
2018-08-13 17:39:24 -0500

    Add a little to gradientDescent

--- visualizer/js/logistic-regression.js
+++ visualizer/js/logistic-regression.js
@@ -1,27 +1,22 @@
 /**
- * https://en.wikipedia.org/wiki/Dot_productct
  * @todo Possibly faster with a for loop.
  */
-function dotProduct(a, b) {
-  return a.map((x, i) => a[i] * b[i]).reduce((m, n) => m + n);
+function dotProduct(as, bs) {
+  return as.map((a, i) => a * bs[i]).reduce((m, n) => m + n);
 }
 
-/**
- * https://en.wikipedia.org/wiki/Sigmoid_function
- */
 function sigmoid(x) {
   return 1 / (1 + Math.exp(-x));
 }
 
 /**
  * Map the independent variable values of a certain data point to a value on the
- * log probability scale (I think). AKA "h(x)".
- * https://ml-cheatsheet.readthedocs.io/en/latest/logistic_regression.html
+ * log probability scale. AKA "h(x)".
  * @param {Number[]} xis An array of independent variable values for one data
  *                       point.
  * @param {Number[]} θs The weights associated with each independent variable.
  */
-function predict(xis, θs) {
+function predictProbability(xis, θs) {
   return sigmoid(dotProduct(xis, θs));
 }
 
@@ -31,6 +26,7 @@ function predict(xis, θs) {
  * -log(1 - h(xis, θs)). The multiplying terms yis and 1 - yis conveniently
  * cancel out whichever side is not applicable.
  * https://en.wikipedia.org/wiki/Cross_entropy
+ * https://ml-cheatsheet.readthedocs.io/en/latest/logistic_regression.html
  * @param {Number[][]} xs An array of number arrays; each number array contains
  *                        the values of the independent variables for one data
  *                        point.
@@ -40,23 +36,35 @@ function predict(xis, θs) {
  * @todo Could be faster with a conditional due to branch prediction, slowness
  *       of multiplication, etc. Also probably faster with a for loop.
  */
-function cost(xs, ys, θs) {
+function calculateCost(xs, ys, θs) {
+  let yi = 0;
   return (
     -xs
-      .map((x, i) => {
-        const xis = xs[i];
-        const yi = ys[i];
+      .map((xis, i) => {
+        yi = ys[i];
         return (
-          yi * Math.log(predict(xis, θs)) +
-          (1 - yi) * Math.log(1 - predict(xis, θs))
+          yi * Math.log(predictProbability(xis, θs)) +
+          (1 - yi) * Math.log(1 - predictProbability(xis, θs))
         );
       })
       .reduce((m, n) => m + n) / xs.length
   );
 }
 
-// function gradientDescent(xs, ys, θs, learningRate) {}
+/* function gradientDescent(xs, ys, θs, learningRate) {
+  const numDataPoints = xs.length;
+  const numIndependent = xs[0].length;
+  let θ = 0;
+  const updatedθs = [].fill.call({ length: xs[0].length + 1 }, 0);
+  for (let i = 0; i < numIndependent; i++) {}
+  xs.map(xi => {});
+}
 
-//function train(data) {
-
-//}
+function train(xs, ys, θs, learningRate, iterations) {
+  let updatedθs = [];
+  for (let i = 0; i < iterations; i++) {
+    updatedθs = gradientDescent(xs, ys, θs, learningRate);
+  }
+  const cost = calculateCost(xs, ys, updatedθs);
+  return { cost, updatedθs };
+} */
